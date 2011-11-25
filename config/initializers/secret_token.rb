6c23603cc232d3d5de67b305802556458a97de44
Danny Connolly <dannyconnolly@email.com>
2011-11-25 12:16:05 +0000

    First Commit, Added a user scaffold and ran migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreetHeartProject::Application.config.secret_token = '7d2e2b5d92aa3dd5ee02e04b3331aaf5ee49a54f47d1e411c43969cb038e3d52ba3a05ed37ef4ad9b72bc6ff20a559edfccb1308bcc5681089dc8bcac19e488f'
