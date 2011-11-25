1b0b4cd2c7ec58fa5a7900457bca73d1cd51c2b6
Danny Connolly <dannyconnolly@email.com>
2011-11-25 12:38:20 +0000

    Added Validation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+	validates :name, :presence => true, :uniqueness => true
+	
+	validates :password, :confirmation => true
+	attr_accessor 	:password_confirmation
+	attr_reader		:password
+	
+	validate :password_must_be_present
+	
+	private
+	
+		def password_must_be_present
+			errors.add(:password, "Please re-type your password") 
+		unless hashed_password.present?
+	end
+	
+	def User.encrypt_password(password, salt)
+		Digest::SHA2.hexdigest(password + "wibble" + salt)
+	end
+	
+	def generate_salt
+		self.salt = self.object_id.to_s +rand. to_s
+	end
 end
