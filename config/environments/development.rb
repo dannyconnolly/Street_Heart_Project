8336ac4e935170ab4132615e203ddb07a04478da
Danny Connolly <dannyconnolly@email.com>
2011-12-05 11:25:36 +0000

    idea file and gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 StreetHeartProject::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -22,5 +23,6 @@ StreetHeartProject::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
 end
 
