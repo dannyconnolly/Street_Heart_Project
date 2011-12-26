1d20e624c3e6dbeed8598eed5accf8e35e407c8e
Siobhan <Siobhan.Schnittger@gmail.com>
2011-12-26 15:16:12 +0000

    function testing email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default :from => "dave@example.com"
+  default :from => "info@streetheart.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class NotifierTest < ActionMailer::TestCase
   test "order_received" do
-    mail = Notifier.order_received
-    assert_equal "Order received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = Notifier.order_received(orders(:one))
+    assert_equal "Street Heart Order Confirmation", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["info@streetheart.com"], mail.from
+    assert_match /1 x Apex Twin/, mail.body.encoded
   end
 
   test "order_shipped" do
-    mail = Notifier.order_shipped
-    assert_equal "Order shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = Notifier.order_shipped(orders(:one))
+    assert_equal "Street Heart Order Shipped", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["info@streetheart.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Apex Twin<\/td>/, mail.body.encoded
   end
 
 end
